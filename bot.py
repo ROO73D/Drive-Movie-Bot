3e7597b139a9528ae395fab102605d3c9fc74b7f
ROO73D <73015074+ROO73D@users.noreply.github.com>
2024-12-08 18:21:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -393,7 +393,7 @@ def handle_all_messages(message):
         else:
           try:
             if 'hubcloud' in i:
-             headers = {
+              headers = {
                  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                  'accept-language': 'en-US,en;q=0.9',
                  'cache-control': 'max-age=0',
